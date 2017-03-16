e7a189d5c53c61fcf0b634cf0e564b636aa6d675
Olof Ekelund <olof.ekelund84@gmail.com>
2017-03-16 23:23:18 +0100

    inventorylist vue component

---
+++ src/App.vue
@@ -0,0 +1,139 @@
+<template>
+  <div id="app">
+    <InventoryList
+      :list="list"
+      @addrow="addRow"
+      @save="save"
+      @delete="deleteRow">
+    </InventoryList>
+  </div>
+</template>
+
+<script>
+import InventoryList from './components/InventoryList'
+
+export default {
+  name: 'app',
+  components: {
+    InventoryList
+  },
+
+  methods: {
+    addRow () {
+      this.idCounter++
+
+      var newItem = {
+        id: this.idCounter,
+        part_nr: '',
+        display_name: '',
+        manufacturer: '',
+        price: ''
+      }
+
+      this.list.push(newItem)
+    },
+
+    save () {
+      // skicka this.list till server och ta emot svar som antingen uppdaterar listan eller
+      // ger ett felmeddelande till användaren. (ifall allting går bra så borde listan vi får tillbaka vara likadan som den som skickades)
+    },
+
+    deleteRow (index) {
+      this.list.splice(index, 1)
+    }
+  },
+
+  created () {
+    this.idCounter = this.list.length
+  },
+
+  data () {
+    return {
+      idCounter: 0,
+
+      list: [
+        {
+          id: 1,
+          part_nr: '6614272-8',
+          display_name: 'Mccray',
+          manufacturer: 'Egestas A Dui Institute',
+          price: '$57.98'
+        },
+        {
+          id: 2,
+          part_nr: '32547852-7',
+          display_name: 'Guerra',
+          manufacturer: 'Sed Pharetra Felis LLP',
+          price: '$59.60'
+        },
+        {
+          id: 3,
+          part_nr: '45740096-9',
+          display_name: 'Mcfarland',
+          manufacturer: 'Sed Neque Foundation',
+          price: '$62.99'
+        },
+        {
+          id: 4,
+          part_nr: '5054940-2',
+          display_name: 'Baxter',
+          manufacturer: 'Ac Mattis Velit Institute',
+          price: '$38.04'
+        },
+        {
+          id: 5,
+          part_nr: '27254124-8',
+          display_name: 'Deleon',
+          manufacturer: 'Integer Vulputate PC',
+          price: '$99.94'
+        },
+        {
+          id: 6,
+          part_nr: '17958397-6',
+          display_name: 'Palmer',
+          manufacturer: 'Massa Non Incorporated',
+          price: '$66.89'
+        },
+        {
+          id: 7,
+          part_nr: '49144112-7',
+          display_name: 'Hobbs',
+          manufacturer: 'Dictum Proin LLC',
+          price: '$8.52'
+        },
+        {
+          id: 8,
+          part_nr: '10975454-4',
+          display_name: 'Rosa',
+          manufacturer: 'Non Sollicitudin Foundation',
+          price: '$53.00'
+        },
+        {
+          id: 9,
+          part_nr: '12502401-7',
+          display_name: 'Hess',
+          manufacturer: 'Phasellus Vitae Associates',
+          price: '$26.40'
+        },
+        {
+          id: 10,
+          part_nr: '50894170-6',
+          display_name: 'Robertson',
+          manufacturer: 'Pede Nonummy Ltd',
+          price: '$65.81'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="sass">
+#app
+  font-family: 'Avenir', Helvetica, Arial, sans-serif
+  -webkit-font-smoothing: antialiased
+  -moz-osx-font-smoothing: grayscale
+  text-align: center
+  color: #2c3e50
+  margin-top: 32px
+</style>
---
+++ src/components/InventoryList.vue
@@ -0,0 +1,173 @@
+<template>
+  <div class="inventoryList">
+    <ul>
+      <li class="headings">
+        <h4 class="idField">Id.</h4>
+        <h4>Name.</h4>
+        <h4>Manufacturer.</h4>
+        <h4>Part nr.</h4>
+        <h4>Price.</h4>
+        <div class="deleteColumn"></div>
+      </li>
+
+      <li v-for="(item, index) in list">
+        <span class="idField">{{ item.id }}</span>
+        <div>
+          <input id="display_name" type="text" v-model="item.display_name">
+        </div>
+        <div>
+          <input id="manufacturer" type="text" v-model="item.manufacturer">
+        </div>
+        <div>
+          <input id="part_nr" type="text" v-model="item.part_nr">
+        </div>
+        <div>
+          <input id="price" type="text" v-model="item.price">
+        </div>
+        <a href="#" class="deleteColumn" @click="remove(index)">
+          <i class="icon fa fa-remove"></i>
+        </a>
+      </li>
+    </ul>
+
+    <div class="controls">
+      <a href="#"
+        class="button"
+        @click="addrow">
+
+        <i class="icon fa fa-plus"/>
+        <span>Add row</span>
+      </a>
+
+      <a href="#"
+        class="button"
+        @click="save">
+        <i class="icon fa fa-save"/>
+        <span>Save</span>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'inventoryList',
+
+  props: {
+    list: {
+      type: Array
+    }
+  },
+
+  methods: {
+    save () {
+      this.$emit('save')
+    },
+
+    addrow () {
+      this.$emit('addrow')
+    },
+
+    remove (index) {
+      this.$emit('delete', index)
+    }
+  },
+
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+
+.inventoryList
+  border: 1px solid #aaa
+  border-radius: 5px
+  box-shadow: 5px 6px 10px -5px rgba(0,0,0,0.4)
+  max-width: 800px
+  margin: 0 auto
+  background: #C5D9E2
+  color: #3e3c3c
+  font-size: 16px
+
+  ul
+    list-style-type: none
+    padding: 0
+    margin: 0
+    border-radius: 3px
+
+  li
+    display: flex
+    padding: 8px
+    justify-content: space-between
+    background: #FEFFFE
+
+    .idField
+      flex: 0 1 2rem
+      align-self: center
+
+    .deleteColumn
+      flex: 0 1 2rem
+      align-self: center
+
+      .icon
+        width: 100%
+        height: 100%
+        color: #3e3c3c
+        transition: color .3s ease
+
+        &:hover
+          color: tomato
+
+    &.headings
+      border-bottom: 1px solid #aaa
+      background: transparent
+
+      > h4
+        margin: 8px 0 0 2px
+
+    > *
+      flex: 1
+      text-align: left
+
+    &:nth-child(even)
+      background-color: #e6edef
+
+    input
+      border: none
+      background: initial
+      transition: background .2s ease
+      padding: 4px
+      width: auto
+      border: 1px solid transparent
+
+      &:hover, &:focus
+        background: #fff
+        border: 1px solid #ccc
+
+  .controls
+    border-top: 1px solid #aaa
+    padding: 16px
+    text-align: right
+
+    .icon
+      padding-right: 4px
+
+    .button
+      background: #fefffe
+      border: 1px solid #aaa
+      border-radius: 5px
+      padding: .5rem 2rem
+      text-transform: uppercase
+      text-decoration: none
+      font-size: .75rem
+      font-weight: 900
+      color: #3e3c3c
+      transition: background .2s ease
+
+      &:hover
+        background: #eee
+
+</style>
